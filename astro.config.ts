6522d7d536249a51767b4ccb5db2038aa139692d
JMarquee85 <joshmarcus85@gmail.com>
2024-01-22 19:43:13 -0700

    Added site base.

--- astro.config.ts
+++ astro.config.ts
@@ -6,6 +6,7 @@ import { visualizer } from 'rollup-plugin-visualizer';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://joshmarcus.dev',
   integrations: [tailwind(), image(), compress()],
   vite: {
     plugins: [visualizer()],
